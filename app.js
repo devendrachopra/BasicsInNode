346b6918c2f8d4770eb2f282b8c772703cad1acc
Devendra Chopra <37116428+devendrachopra@users.noreply.github.com>
2022-12-03 15:42:16 +0000

    DB Connecrtion & User Schema & Registration

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 require('dotenv').config()
+require("./config/database").connect()
 const express = require("express")
+const User = require("./model/user")
+const bcrypt = require("bcryptjs")
+var cookieParser = require('cookie-parser')
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -7,6 +11,53 @@ app.use(express.urlencoded({ extended: true }))
 
 app.get('/', function (req, res) {
     res.send('Hello World')
+})
+
+app.post("/register", async (req, res)=>{
+    try {
+        //collect all information
+        const {firstname, lastname, email, password } = req.body
+        //validate the data, if exists
+        if (!(email && password && lastname && firstname)) {
+            res.status(401).send("All fileds are required")
+        }
+        //check if email is in correct format
+
+        //check if user exists or not
+        const existingUser = await User.findOne({ email})
+        if (existingUser) {
+            res.status(401).send("User already found in database")
+        }
+
+        //encrypt the password
+        const myEncyPassword = await bcrypt.hash(password, 10)
+
+       
+        //create a new entry in database
+        const user = await User.create({
+            firstname,
+            lastname,
+            email,
+            password: myEncyPassword,
+        })
+        
+        //create a token and send it to user
+        const token = jwt.sign({
+            id: user._id, email
+        }, 'shhhhh', {expiresIn: '2h'})
+        
+
+        user.token = token
+        //don't want to send the password
+        user.password = undefined
+
+        res.status(201).json(user)
+
+
+    } catch (error) {
+        console.log(error);
+        console.log("Error is response route");
+    }
 })
 
 module.exports = app
---
+++ config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const MONGODB_URL = process.env.MONGODB_URL
+
+exports.connect = () =>{
+    mongoose.connect(MONGODB_URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(console.log('DB connected successfully'))
+    .catch(err=>{
+        console.log('DB connection failed!');
+        console.log(err);
+        process.exit(1)
+    })
+}
